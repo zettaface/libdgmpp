92fc862253b3ba20ab6d9f762f5fbaace702e22f
Vladimir Efimov <zettaface@gmail.com>
2017-12-03 17:02:19 +0300

    Return type fix

--- Compiler/Compiler.h
+++ Compiler/Compiler.h
@@ -150,11 +150,11 @@ namespace Compiler {
 		}
 		
 		template<typename T>
-		void is() {
+		bool is() {
 			return (type_id == typeid(T).hash_code());
 		}
 		
-		void valid() {
+		bool valid() {
 			return (type_id != invalid_type());
 		}
 		
